befccecb166ecb8af1328c3b27c62f4fef2cc70d
Johan Borgström <johan@johans-mac-pro.lan>
2015-04-05 14:49:44 +0200

    use parent

--- petfactory/util/search.py
+++ petfactory/util/search.py
@@ -1,41 +1,49 @@
 import re
-import pprint
+#import pprint
+
+def create_set_from_regex(pattern, nodetype, use_longname, use_parent, set_name):
+    
+    node_list = pm.ls(type=nodetype)
+    
+    match_list = []
+    for node in node_list:
+        
+        if use_parent:
+            
+            try:
+                node = node.getParent()
+                
+            except AttributeError as e:
+                pm.warning(e)
+                continue
+
+        if use_longname:
+            name = node.longName()
+        else:
+            name = node.nodeName()         
+        #print(name)
+        
+        match_obj = re.match(pattern, name)
+        if match_obj:
+            match_list.append(node)
+
+    #pprint.pprint(match_list)
+    pm.select(deselect=True)
+    mesh_set = pm.sets(name='{0}_set'.format(set_name))
+    mesh_set.addMembers(match_list)
+    
 
 
 pattern = r'^[\w\d|]*weld[\w\d|]*$'
 nodetype = pm.nodetypes.Mesh
 use_longname = True
-use_shape = True
-match_list = []
+use_parent = True
 set_name = 'weld'
-node_list = pm.ls(type='transform')
 
-for node in node_list:
-    
-    if use_longname:
-        name = node.longName()
-    else:
-        name = node.nodeName()         
-    #print(name)
-    
-    match_obj = re.match(pattern, name)
-    if match_obj:
-        
-        if use_shape:
-            
-            try:
-                if isinstance(node.getShape(), nodetype):
-                    match_list.append(node)
-                
-            except AttributeError as e:
-                #pm.warning('Select a transform to use the getShape method. {0}'.format(e))
-                continue
-        else:
-            match_list.append(node)
-            
 
-#pprint.pprint(match_list)
+create_set_from_regex(  pattern=pattern,
+                        nodetype=nodetype,
+                        use_longname=use_longname,
+                        use_parent=use_parent,
+                        set_name=set_name)
 
-pm.select(deselect=True)
-mesh_set = pm.sets(name='{0}_set'.format(set_name))
-mesh_set.addMembers(match_list)
