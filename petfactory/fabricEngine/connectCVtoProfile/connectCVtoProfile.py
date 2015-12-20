aa78ae24bdf5435e78694d6198d57d866a8b5561
Johan Borgström <johan@petfactory.se>
2015-12-20 17:23:09 +0100

    add twoCirctangent setup

--- petfactory/fabricEngine/connectCVtoProfile/connectCVtoProfile.py
+++ petfactory/fabricEngine/connectCVtoProfile/connectCVtoProfile.py
@@ -9,25 +9,39 @@ reload(simple_widget)
 
 def connect_selected_crv(crv_xfo, canvas_node):
     
-    crv_shape = crv_xfo.getShape()
-    crv_cvs = crv_shape.getCVs()
+    try:
+        crv_shape = crv_xfo.getShape()
+        crv_cvs = crv_shape.getCVs()
     
-    canvas_node.profileCount.set(len(crv_cvs))
+    except AttributeError as e:
+        pm.warning('{}'.format(e))
+        return
+        
     
-    for index, cv in enumerate(crv_cvs):
-        crv_shape.controlPoints[index] >> canvas_node.profile[index]
+    try:
+        canvas_node.sideProfileCount.set(len(crv_cvs))
+        
+        for index, cv in enumerate(crv_cvs):
+            crv_shape.controlPoints[index] >> canvas_node.sideProfile[index]
+            
+    except AttributeError as e:
+        pm.warning('{}'.format(e))
         
         
 
 def remove_input_crv(canvas_node):
     
-    num = canvas_node.profile.numElements()
+    try: 
+        num = canvas_node.sideProfile.numElements()
     
-    for i in range(num):
-        if canvas_node.profile[i].isConnected():
-           canvas_node.profile[i].disconnect()
-           
-    canvas_node.profileCount.set(0)
+        for i in range(num):
+            if canvas_node.sideProfile[i].isConnected():
+               canvas_node.sideProfile[i].disconnect()
+               
+        canvas_node.sideProfileCount.set(0)
+        
+    except AttributeError as e:
+        pm.warning('{}'.format(e))
     
     
     
@@ -44,15 +58,22 @@ class Panel(QtGui.QWidget):
         
         self.setWindowFlags(QtCore.Qt.Tool)
         self.setGeometry(200, 200, 250, 100)
-        self.setWindowTitle('Panel')
+        self.setWindowTitle('TwoCircleTangent')
         
         vbox = QtGui.QVBoxLayout()
         self.setLayout(vbox)
         
+        # create canvas node
+        create_canvas_node_btn = QtGui.QPushButton('Create canvas node')
+        vbox.addWidget(create_canvas_node_btn)
+        create_canvas_node_btn.clicked.connect(self.create_canvas_node_btn_clicked)
+        
+        
+        # canvas node lineedit
         self.canvas_lineedit = simple_widget.add_filtered_populate_lineedit(label='  canvasNode >>  ', parent_layout=vbox, nodetype=pm.nodetypes.CanvasNode)
-        
-        # connect
-        
+           
+                
+        # connect        
         connect_cvs_btn = QtGui.QPushButton('Connect CVs to profile')
         vbox.addWidget(connect_cvs_btn)
         connect_cvs_btn.clicked.connect(self.connect_cvs_btn_clicked)
@@ -61,8 +82,7 @@ class Panel(QtGui.QWidget):
         p.setColor(connect_cvs_btn.backgroundRole(), QtGui.QColor(0,100,0))
         connect_cvs_btn.setPalette(p)
 
-        # break
-        
+        # break connections        
         break_con_btn = QtGui.QPushButton('Break connection')
         vbox.addWidget(break_con_btn)
         break_con_btn.clicked.connect(self.break_con_btn_clicked)
@@ -73,6 +93,13 @@ class Panel(QtGui.QWidget):
            
         
         vbox.addStretch()
+        
+    def create_canvas_node_btn_clicked(self):
+        
+        canvasNode = pm.createNode('canvasNode')
+        path = '/Users/johan/Dev/fabricEngine/canvas/polygon/twoCircleTangent/twoCircleTangentXfoFlip.canvas'
+        pm.dfgImportJSON(m=canvasNode, f=path)
+        self.canvas_lineedit.setText(canvasNode.name())
         
     def break_con_btn_clicked(self):
         
@@ -91,6 +118,5 @@ class Panel(QtGui.QWidget):
 def show():
     win = Panel(parent=maya_main_window())
     win.show()
-    
-    
-show()
+
+#show()
