9ed8edeb6a4af5d1874331b7a7f8478c04503142
Johan Borgström <johan@petfactory.se>
2016-01-08 14:56:28 +0100

    set matrix to main_grp

--- petfactory/fabricEngine/connectCVtoProfile/buildMeshOutput.py
+++ petfactory/fabricEngine/connectCVtoProfile/buildMeshOutput.py
@@ -3,30 +3,59 @@
 def buildMeshOutput():
 
     sel_list = pm.ls(sl=True)
-    mesh_list = []
-    
+   
     for sel in sel_list:
         
         if not isinstance(sel, pm.nodetypes.CanvasNode):
             print('"{}" is not a canvas node, skipping...'.format(sel))
             continue
         
-        attr_list = sel.listAttr(userDefined=True, settable=True, readOnly=True)
+        main_grp = pm.group(em=True)
+        
+        matrix = None
+        name = None
+        attr_list = sel.listAttr(userDefined=True, settable=True)
+        
+        for attr in attr_list:
+            
+            # skip compund attr
+            if attr.isChild():
+                continue
                 
+            if attr.type() == 'matrix':
+                print('matrix')
+                matrix = attr.get()
+                main_grp.setMatrix(matrix)
+                
+                inputs = attr.inputs()
+                if inputs:
+                    name = inputs[0].name()
+                    main_grp.rename('{}_grp'.format(name))
+                                   
+        attr_list = sel.listAttr(userDefined=True, settable=True, readOnly=True)
+          
         for attr in attr_list:
             
             if attr.type() != 'mesh':
                 continue
-                
+                  
             attr_name = attr.name(includeNode=False)
             mesh = pm.createNode('mesh')
             attr >> mesh.inMesh
-            dup_mesh = pm.duplicate(mesh)
-            mesh_list.append(dup_mesh)
+            
+            dup_trans = pm.duplicate(name='{}_mesh'.format(name))
+            
+            # break connection to the canvas node and delete
             attr // mesh.inMesh
             pm.delete(mesh.getParent())
-        
-    pm.select(mesh_list)
-    
+            
+            #mesh_trans = pm.group(em=True, name='{}_mesh'.format(name))
+            #pm.parent(mesh, mesh_trans, shape=True, add=True)
+            
+            pm.parent(dup_trans, main_grp)
+            
+            
+            
 
+#pm.select('canvasNode2')
 buildMeshOutput()
