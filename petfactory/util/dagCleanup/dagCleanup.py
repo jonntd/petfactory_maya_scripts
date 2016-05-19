ec0e0a319ad34465a9f9b7420511b1181f3e16c6
Johan Borgström <johan@petfactory.se>
2016-05-19 14:30:21 +0200

    add indicator gif

--- petfactory/util/dagCleanup/dagCleanup.py
+++ petfactory/util/dagCleanup/dagCleanup.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from PySide import QtCore, QtGui
 import maya.OpenMayaUI as omui
 from shiboken import wrapInstance
+import os
 
    
 def getValidNodes(node, shapeDagSet, removeHidden):
@@ -52,27 +53,16 @@ def deleteNotInSet(node, shapeDagSet, deleteList):
     for child in childList:    
         deleteNotInSet(child, shapeDagSet, deleteList)
 
-
-def cleanup(root, removeHidden, selectNodes):
-
-    st = datetime.now()
-    shapeDagSet = set()
-    getValidNodes(root, shapeDagSet, removeHidden)
-    deleteList = []
-    deleteNotInSet(root, shapeDagSet, deleteList)
-    if selectNodes:
-        pm.select(deleteList)
-    else:
-        pm.delete(deleteList)
-    et = datetime.now()
-    print 'The hierarchy cleanup took: {} seconds'.format((et-st).seconds)
     
 
 def maya_main_window():
     main_window_ptr = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window_ptr), QtGui.QWidget)
     
-              
+class Communicate(QtCore.QObject):
+    
+    cleanupDone = QtCore.Signal(int)
+            
 class MeasureWidget(QtGui.QWidget):
     
     def __init__(self, parent=None):
@@ -83,6 +73,9 @@ class MeasureWidget(QtGui.QWidget):
         self.setGeometry(200, 200, 180, 100)
         self.setWindowTitle('DAG Cleanup')
         
+        self.c = Communicate()
+        self.c.cleanupDone.connect(self.cleanupDone)
+        
         vbox = QtGui.QVBoxLayout(self)
         
         self.deleteHiddenCB = QtGui.QCheckBox('Delete Hidden')
@@ -92,10 +85,42 @@ class MeasureWidget(QtGui.QWidget):
         vbox.addWidget(self.selectNodesCB)
         
         vbox.addStretch()
+        
+        self.progressLabel = QtGui.QLabel()
+        self.progressLabel.setAlignment(QtCore.Qt.AlignHCenter)
+        
+        #self.movie = QtGui.QMovie(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'indicator.gif'))
+        self.movie = QtGui.QMovie(os.path.join('/Users/johan/Desktop', 'indicator.gif'))
+        self.progressLabel.setMovie(self.movie)
+        vbox.addWidget(self.progressLabel)
+        
+        
         cleanupButton = QtGui.QPushButton('Cleanup')
         cleanupButton.clicked.connect(self.cleanupButtonClicked)
         vbox.addWidget(cleanupButton)
+        
 
+    def cleanupDone(self, duration):
+        print 'The hierarchy cleanup took: {} seconds'.format(duration)
+        self.movie.stop()
+        self.progressLabel.setMovie(None)
+        
+    def cleanup(self, root, removeHidden, selectNodes):
+
+        st = datetime.now()
+        shapeDagSet = set()
+        getValidNodes(root, shapeDagSet, removeHidden)
+        deleteList = []
+        deleteNotInSet(root, shapeDagSet, deleteList)
+        if selectNodes:
+            pm.select(deleteList)
+        else:
+            pm.delete(deleteList)
+        et = datetime.now()
+
+        self.c.cleanupDone.emit((et-st).seconds)
+    
+    
     def cleanupButtonClicked(self):
 
         selList = pm.ls(sl=True)
@@ -104,9 +129,11 @@ class MeasureWidget(QtGui.QWidget):
             return
         
         
-        cleanup(selList[0],
-                removeHidden=self.deleteHiddenCB.isChecked(),
-                selectNodes=self.selectNodesCB.isChecked())
+        self.movie.start()
+           
+        self.cleanup(   selList[0],
+                        removeHidden=self.deleteHiddenCB.isChecked(),
+                        selectNodes=self.selectNodesCB.isChecked())
         
         
 def show():
